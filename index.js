049821ceba77575f03716e1a5e51eaea14f03a95
AMFahim <amfahim2019@gmail.com>
2021-07-26 00:07:32 +0600

    basic type

---
+++ index.js
@@ -0,0 +1,1 @@
+var myName = "Abdul Mozid Fahim";
---
+++ index.ts
@@ -0,0 +1,8 @@
+// dataType = Any, number, string, boolean, object, Array, Tuple, Enum, undefined, null, void, never, unknown
+
+let myName: string = "Abdul Mozid Fahim";
+
+let age: number;
+age = 16;
+
+let isMarried: boolean = false;
