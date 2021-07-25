c1d861e501b1c945c2800de635093ba0939576c5
AMFahim <amfahim2019@gmail.com>
2021-07-26 00:53:18 +0600

    object type

--- index.ts
+++ index.ts
@@ -18,4 +18,21 @@ studentId = "amf-324";
 
             //---------------------Object--------------------
 
-            
+type PersonType = {name: string, age: number, hobby: string}       
+interface Person {
+  name : string;
+  age  : number;
+  hobby?: string
+}     
+
+let person: Person ={
+  name : "Abdul Mozid Fahim",
+  age  : 16,
+  hobby: "Sleeping"
+}   
+
+let person2: Person ={
+  name : "Someone",
+  age  : 11,
+  hobby: "traveling"
+}
