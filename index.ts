2fa60fa23cb3b079bb4c44b558d5961368202666
AMFahim <amfahim2019@gmail.com>
2021-07-26 00:27:02 +0600

    union type

--- index.ts
+++ index.ts
@@ -6,3 +6,16 @@ let age: number;
 age = 16;
 
 let isMarried: boolean = false;
+
+
+
+            //---------------------union type----------------------
+
+type NumberOrString = number | string
+// let studentId: number | string = 10876;
+let studentId: NumberOrString = 433454;
+studentId = "amf-324"; 
+
+            //---------------------Object--------------------
+
+            
